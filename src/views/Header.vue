7c870b9148cd26cb97a28c99f6eb5cad1fc6e8ed
zmy <111@qq.com>
2020-06-17 16:33:25 +0800

    第一次提交

---
+++ src/views/Header.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="about">
+      <div class="back">
+        <span class="iconfont">&#xe652;</span>
+      </div>        
+      <el-input v-model="city" placeholder="输入城市/景点/游玩主题"></el-input>
+      <div class="city">
+        <span>上海</span>
+        <i class="iconfont">&#xe658;</i>
+      </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeHeader',
+  data(){
+    return{
+      city:""
+    }
+  }
+}
+</script>
+<style scoped>
+  .about{
+    width: auto;
+    height: 44px;
+    color: #333;
+    background-color: #00bcd4;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .back{
+    width: 5%;
+    
+  }
+  .back .iconfont{
+    font-size: 24px;
+  }
+  .about >>> .el-input__inner{
+    width: 100%;
+    height: 30px;
+  }
+  .city{
+    width: 8%;
+  }
+</style>
---
+++ src/views/Home/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <HomeHeader></HomeHeader>
+    <Icon></Icon>
+  </div>
+</template>
+
+<script>
+import HomeHeader from '../Header'
+import Icon from './Icon.vue'
+
+export default {
+  name: 'Home',
+  components: {
+   HomeHeader,
+   Icon
+  }
+}
+</script>
+<style scoped>
+  .home{
+    width:auto;
+    height:1000px;
+  }
+</style>
